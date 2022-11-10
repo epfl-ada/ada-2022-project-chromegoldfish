e15956fc38fe5a6897a5c3bab17419b20d2eb4d8
Simonlee711 <simonlee711@gmail.com>
2022-11-10 08:28:00 -0800

    updates to notebook. trying to cluster rn

---
+++ simon/json-to-csv.py
@@ -0,0 +1,8 @@
+import json
+import pandas as pd
+from pandas.io.json import json_normalize
+f = open('../data/yt_metadata_en.jsonl.gz') # open the json file
+data = json.load(f) # load as json
+f.close()
+df = json_normalize(data['features']) #load json into dataframe
+df.to_csv('json-to-csv.csv', sep=',', encoding='utf-8') #save as csv
